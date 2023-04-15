3e1d740ea12d994fe13f5c6d208be06657e6c3e5
JuniorDaniel01 <danieljr21@outlook.com>
2023-04-15 01:58:41 -0300

    menu_responsivo_atualizacao

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,6 @@ function menuShow() {
     if(menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
         document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
-
     } else {
         menuMobile.classList.add('open');
         document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
